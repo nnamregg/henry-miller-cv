7d34d8a660310b97533eec1a64758ae090f90253
nnamregg <nnamreg.t@gmail.com>
2023-01-28 23:32:45 -0300

    Funcionalidad en togglers de secciones de Cine.

--- js/movies.js
+++ js/movies.js
@@ -1,34 +1,16 @@
-// const asHimselfToggler = document.getElementById('movies_self_expand')
-// const byActorsToggler = document.getElementById('movies_actors_expand')
 
-const sections = document.querySelectorAll('.movies_section')
-const togglers = document.querySelectorAll('.movies_toggle')
+const buttons = document.getElementsByClassName("movies_collapsible");
 
+for (i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        const content = this.parentNode.previousSibling.previousSibling;
+        
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+  });
 
-function isSectionExpanded(id) {
-    // console.log('id -> ', id)
-    const idStr = id.slice(0, id.indexOf('_'))
-    // console.log('idStr -> ', idStr)
-
-    const section = document.getElementById(`miller_${idStr}`);
-    const classes = section.classList
-    classes.toggle('expanded')
-
-}
-
-window.onload = () => {
-    console.log('togglers -> ', togglers)
-    
-    for(let toggler of togglers) {
-        toggler.addEventListener('click', (e) => {
-            // console.log(e)
-            isSectionExpanded(e.target.id)
-
-        })
-    }
-}
-/* 
-asHimselfToggler.addEventListener('click', (e) => {
-    console.log('event -> ', e)
-
-}) */
+} 
